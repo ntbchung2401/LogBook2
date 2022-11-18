b17a5f9381e0cf017dd3d0531aac08c5d3de2236
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2022-11-19 01:50:20 +0700

    logbook2

--- app/src/main/java/com/example/logbook2/MainActivity.java
+++ app/src/main/java/com/example/logbook2/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private  int index = 0;
     EditText inputURL;
     public String url;
+    private static final String validateURL = "(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpeg|jpg|gif|png)";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,34 +35,38 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.img_view);
         imageList = new ArrayList<>();
-
         backward = findViewById(R.id.backward);
         forward = findViewById(R.id.forward);
         inputURL = findViewById(R.id.inputURL);
         add_URL = findViewById(R.id.add_URL);
-
         imageList.add(0,"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg");
         imageList.add(1,"https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg");
         imageList.add(2,"https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg");
         imageList.add(3,"https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg");
 
-
         add_URL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String url = inputURL.getText().toString().trim();
-                if(url.length() !=0 ) {
-                    Glide.with(MainActivity.this)
-                            .load(url)
-                            .apply(new RequestOptions().transform(new CenterCrop()).transform(new RoundedCorners(12)))
-                            .into(imageView);
-                imageList.add((url));
-                Toast.makeText(MainActivity.this, "Add successfully!",Toast.LENGTH_SHORT).show();
+                if(url.length()>0){
+                    if(url.matches(validateURL) ) {
+                        Glide.with(MainActivity.this)
+                                .load(url)
+                                .apply(new RequestOptions().transform(new CenterCrop()).transform(new RoundedCorners(12)))
+                                .into(imageView);
+                        imageList.add((url));
+                        inputURL.getText().clear();
+                        Toast.makeText(MainActivity.this, "Add successfully!",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, "Invalid",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Empty url",Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
     }
-
     public void previousImage(View view) {
         index++;
         if(index >= imageList.size()){
@@ -69,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         }
         loadImage();
     }
-
     public void nextImage(View view) {
         index --;
         if(index <= -1){
@@ -77,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
         }
         loadImage();
     }
-
     private void loadImage(){
         Glide.with(MainActivity.this)
                 .load(imageList.get(index))
